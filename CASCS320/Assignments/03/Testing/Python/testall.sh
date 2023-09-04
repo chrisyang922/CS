9e97edb78dc89b7f1b09e05d4033e239dab1c27b
chrisyang922 <31582082+chrisyang922@users.noreply.github.com>
2023-09-04 14:52:08 -0400

    Delete CASCS320/Assignments/03 directory

--- CASCS320/Assignments/03/Testing/Python/testall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-python3 < assign03_03_test.py
-python3 < assign03_06_test.py
-
-###### end of [testall.sh] ######
--- CASCS320/Assignments/03/Testing/SML/testall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sml < assign03-01-test.sml
-sml < assign03-02-test.sml
-sml < assign03-04-test.sml
-sml < assign03-05-test.sml
-sml < assign03-08-test.sml
-
-###### end of [testall.sh] ######
